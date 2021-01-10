faf75453904cd74df808adc9b3d581e9acaa979c
peatasura <keenphantom.2013@gmail.com>
2021-01-10 22:19:18 +0700

    [opopopowod]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer , useState } from "react";
 import CourseCard from "./components/CourseCard";
 import CourseForm from "./components/CourseForm";
 
+export const CardContext = createContext({});
+
 function App() {
+  const [GPA, setGPA] = useState(0.00);
+  function calculateGPA() {
+    // TODO
+    let grade = 0.00
+    let credit = 0
+    state.currentState.forEach(obj => {
+      if(obj.a !== "W"){
+        grade += Number(obj.g) * Number(obj.c)
+        credit += Number(obj.c)
+      }
+    })
+    grade  = Number(grade) / Number(credit)
+    if(Number(grade) >= 0)
+      setGPA(Number(grade.toPrecision(3)))
+    else setGPA(0.00)
+  }
+
+  const reducer = (state , action) =>{
+    switch(action.type){
+      case "ADD_CARD":
+        return {
+          ...state,
+          currentState: [...state.currentState, action.payload],
+        };
+      case "SET_CARD":
+        return {
+          ...state,
+          currentState: action.payload,
+        };
+      case "DELETE_CARD":
+        return {
+          ...state,
+          currentState: state.currentState.filter((card) => card.id !== action.payload),
+        };
+      }
+  }
+
+  const [state,dispatch] = useReducer(reducer, {currentState:[],})
+  function fetchCards() {
+    const localState = localStorage.getItem("currentState");
+    if (localState) {
+      dispatch({
+        type: "SET_CARD",
+        payload: JSON.parse(localState),
+      })
+    }
+    calculateGPA()
+  }
+
+  useEffect(fetchCards, [])
+  useEffect(() => {
+    localStorage.setItem("currentState", JSON.stringify(state.currentState));
+    calculateGPA()
+  }, [state.currentState])
+
   return (
-    <div>
-      <h1>GPA CALCULATOR</h1>
-      {/* TODO ADD UI */}
-    </div>
+    <CardContext.Provider value = {{state,dispatch}}>
+      <div>
+        <h1 class ="headone">GPA CALCULATOR V.3</h1>
+        <CourseCard state={state.currentState} />
+      </div>
+      <CourseForm />
+      
+      <p id = "show"class ="show" align = "center">Your GPA is {GPA.toFixed(2)}</p>
+    </CardContext.Provider>
   );
 }
 
